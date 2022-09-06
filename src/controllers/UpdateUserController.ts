aae37b4737b6cca422a3029d0c097a46b7610918
Firulais <javimario69@gmail.com>
2022-09-06 20:32:39 -0300

    Mi Primer Commit

---
+++ src/controllers/UpdateUserController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { UpdateUserService } from "../services/UpdateUserService";
+
+class UpdateUserController {
+  async handle(request: Request, response: Response) {
+    const { id, username, email, telefone, cidade, estado } = request.body;
+
+    const updateUserService = new UpdateUserService();
+
+    try {
+      await updateUserService.update({ id, username, email, telefone, cidade, estado }).then(() => {
+        response.render("message", {
+          message: "Usuario actualizado correctamente"
+        });
+      });
+    } catch (err) {
+      response.render("message", {
+        message: `Error al actualizar usuario: ${err.message}`
+      });
+    }
+
+  }
+}
+
+export { UpdateUserController };
